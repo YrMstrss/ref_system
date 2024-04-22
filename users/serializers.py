532ebb1a684f9abe08242940c903ecd1171c7d36
ValeryYaku <ya.lera55555@gmail.com>
2024-04-22 13:20:54 +0300

    created serializer for view user profile

--- users/serializers.py
+++ users/serializers.py
@@ -7,3 +7,9 @@ class UserAuthSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('phone', )
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('phone', 'invite_code', 'inviter_code',)
