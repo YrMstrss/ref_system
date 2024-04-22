df552d4a8a963704948359d1d3c725b02fc5e5af
ValeryYaku <ya.lera55555@gmail.com>
2024-04-22 18:28:29 +0300

    docker

--- referral_system/settings.py
+++ referral_system/settings.py
@@ -89,8 +89,8 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': 'db',
-        'PORT': 5432
+        # 'HOST': 'db',
+        # 'PORT': 5432
     }
 }
 
