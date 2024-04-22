2f96b3ba8a7d9b75b30b36274366efbeff45835c
ValeryYaku <ya.lera55555@gmail.com>
2024-04-22 17:21:50 +0300

    docker files

--- referral_system/settings.py
+++ referral_system/settings.py
@@ -88,7 +88,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD')
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
