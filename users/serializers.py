727b1d13b26969f312598c54014bb60de79d0415
ValeryYaku <ya.lera55555@gmail.com>
2024-04-22 14:49:55 +0300

    added field invited users

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
 from users.models import User
 
@@ -10,6 +11,14 @@ class UserAuthSerializer(serializers.ModelSerializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
+    invited_users = SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ('phone', 'invite_code', 'inviter_code',)
+        fields = ('phone', 'invite_code', 'inviter_code', 'invited_users')
+
+    def get_invited_users(self, instance):
+        try:
+            return User.objects.get(inviter_code=instance.invite_code)
+        except User.DoesNotExist:
+            return None
